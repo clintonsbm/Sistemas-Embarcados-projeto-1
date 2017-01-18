88dc20ab34efd6c58bbdae8329a6a4c30fea723b
Clinton de Sá Barreto Maciel <clintonsbm@gmail.com>
2017-01-18 00:24:57 -0300

    WriteMsg changes and rename

--- writeMsg.c
+++ writeMsg.c
@@ -9,54 +9,72 @@ void encode_length(FILE *, FILE *, int);
 void encode_message(FILE *, FILE *, int, char *, int, int);
 
 int main(int argc, char **argv) {
+
+  //If the program has less than two parameters returns 1 and stop
   if(argc < 3) {
     printf("Program requires 2 parameters to work properly; the message within \" \" and the PPM file you want to encode the message in.\nAborting\n.");
     return 1;
   }
 
   FILE *fp;
+  //If the program couldn't open the file returns 1 and stop
   if((fp = fopen(argv[2], "r+")) == NULL) {
     printf("Could not open file %s.\nAborting\n", argv[2]);
     return 1;
-  } 
-  
+  }
+
   if(read_ppm_type(fp)) {
-    printf("\nStart!\n\n");
+    printf("\nStart aplication!\n");
+
+    //Skip comments inside the ppm file
     skip_comments(fp);
-    printf("\nFinished skipping\n");
 
+    //Load message to be encrypted
     char *myMessage = (char *)argv[1];
-	//myMessage = " aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-	//myMessage = "asdasd";    
-	int message_length = get_message_length(myMessage);
+	  //myMessage = " aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
+    //Get message length
+	  int message_length = get_message_length(myMessage);
+
+    //Get file Width and Height
     int w = get_width(fp);
     int h = get_height(fp);
-	printf("\nWe've got the sizes \n\n");
 
+	  printf("\nImage sized defined\n");
+
+    //Based on the image size defines if is possible to hide the message
     if(message_fits(message_length, w, h)) {
-	  printf("\nStart writing\n\n");
+	    printf("\nImage capable of hidding message\n");
+
+      //Read color depth of the image. If less than 255, returns 0
+      //The algorithm is prepared to handle with Direct Colors (8 bits images)
       if(read_color_depth(fp)) {
+        //Creates output file
       	FILE *fp_t = fopen("out.ppm","w");
-      	int i = count_new_lines(fp);  
+
+        //
+      	int i = count_new_lines(fp);
+
+        //
       	copy_header(fp, i, fp_t);
         encode_length(fp, fp_t, (message_length - 8) / 8);
       	encode_message(fp, fp_t, (message_length - 8), myMessage, w, h);
+
       	printf("Encoding Process Complete. Take a look at out.ppm\n");
-      	
+
+        //Close files
       	fclose(fp);
       	fclose(fp_t);
-		printf("\nFinished writing\n\n");
       } else {
-	printf("\nError: Image color depth invalid. Must be 255.\n");
-	return 1;
+	        printf("\nError: Image color depth invalid. Must be 255.\n");
+	        return 1;
       }
     } else {
-      printf("\nError: Image file is not large enough to hold the message.\nAborting.\n");
+      printf("\nError: Image file is not large enough to hold the message.\n\nAborting.\n");
       return 1;
     }
   } else {
-    printf("Error: Wrong file format.\nAborting\n");
+    printf("Error: Wrong file format.\n\nAborting\n");
     return 1;
   }
   return 0;
@@ -65,57 +83,74 @@ int main(int argc, char **argv) {
 void copy_header(FILE *fp1, int numLines, FILE *fp2) {
   int i;
   char temp;
-  
-  rewind(fp1); //Goes back to the beginning of the file
-  printf("\nStarted copy_header\n\n");
+
+  //Goes back to the beginning of the file
+  rewind(fp1);
+
+  printf("\nCopying header\n");
+
+  //Copies the header of the file
   for(i = 0; i < numLines; i++) {
     while((temp = fgetc(fp1)) != EOF && temp != '\n') {
       fputc(temp, fp2);
     }
     fputc('\n', fp2);
   }
-  
-  printf("\nEnded copy_header\n\n");
+
+  //printf("\nEnded copy_header\n\n");
   return;
 }
 
+//Get message length
 int get_message_length(char my_msg[]) {
   int i = 0;
   while(my_msg[i] != '\0') {
     i++;
   }
+
+  //Returns the number of bites necessary to encode the message
+  //8 per char and jumps the first char (junk)
   return i * 8 + 8;
 }
 
+//Check if the message it's smaller than the file size
 int message_fits(int length, int w, int h) {
   return length < w * h * 3;
 }
 
+//Count new lines on the header******
 int count_new_lines(FILE *fp) {
-  char temp; int count = 0;
+  char temp;
+  int count = 0;
 
+  //Goes back to the beginning of the file
   rewind(fp);
 
-  while((temp = fgetc(fp)) != EOF) { 
-	printf("%d", count);
+  while((temp = fgetc(fp)) != EOF) {
+	//printf("%d", count);
 	if (count == 4) {
-      return count;  
+      return count;
     }
-    if(temp == '\n') { 
+    if(temp == '\n') {
       count++;
-	} 
+	}
   }
 
-  return count; 
+  return count;
 }
 
+//
 void encode_length(FILE *in, FILE *out, int length) {
-  char temp; 
+  char temp;
   int i, l;
-  printf("\nStarted encode_length\n\n");
+
+  printf("\nEncoding length\n");
+
+  //Encodes the message leng on the first byte ******
+  //(l & 1) is the operator AND in binari
   for(i = 0; i < 8; i++) {
-    temp = fgetc(in);
-      l = length; 
+      temp = fgetc(in);
+      l = length;
       l >>= 7 - i;
       if((l & 1) == 1) {
       	if((temp & 1) == 0) {
@@ -129,23 +164,27 @@ void encode_length(FILE *in, FILE *out, int length) {
     fputc(temp, out);
   }
   printf("%d", length);
-  printf("\nEnded encode_length\n\n");
+
   return;
 }
 
 void encode_message(FILE *in, FILE *out, int num_to_encode, char* my_message, int w, int h) {
   int encoded = 0;
   unsigned char temp;
-  int idx = 0, num_coppied = 0; 
-  char current; 
+  int idx = 0, num_coppied = 0;
+  char current;
 
   int fileSize = (w * h * 3) - 8; //Number of bytes after first 8
   int i;
-  printf("\nStarted encode_message\n\n");
+
+  printf("\nEncoding message\n");
+
+  //Starts encoding the message by going char by char of the file and changing
+  //the less significant bit
   for(i = 0; i < fileSize; i++) {
     temp = fgetc(in);
     current = my_message[idx];
-    
+
     current >>= 7 - num_coppied;
     num_coppied++;
 
@@ -165,10 +204,9 @@ void encode_message(FILE *in, FILE *out, int num_to_encode, char* my_message, in
       	num_coppied = 0;
       }
     }
-    
+
     fputc(temp, out);
   }
-  
-  printf("\nEnded encode_message\n\n");
+
   return;
 }
